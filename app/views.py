466226a1f6aa8c538a9f61cc28a453654004448f
William <wostensen96@gmail.com>
2016-04-17 00:05:18 +0200

    trying out forms

--- app/views.py
+++ app/views.py
@@ -1,20 +1,20 @@
 from app import app
-from flask import render_template
+from flask import render_template, flash, redirect
+from .forms import LoginForm
 
 @app.route('/')
 @app.route('/index')
 def index():
  user = "William"
- posts = [
- { 
-  'author':  {'nickname': 'John'},
-  'body': 'Beautiful day!'
- },
- {
-  'author': {'nickname': 'Susan'},
-  'body': 'Cool movie'
- }
- ]
- return render_template('index.html',
-   user=user,
-   posts=posts)
+ posts = ()
+ return render_template('index.html', user=user, posts=posts)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+ form = LoginForm()
+ if form.validate_on_submit():
+  flash('Login requested for OpenID="%s", rememberme=%s' % (form.openid.data, str(form.remember_me.data)))
+  return redirect('/index')
+ return render_template('login.html',
+  title='Sign Up',
+  form=form)
